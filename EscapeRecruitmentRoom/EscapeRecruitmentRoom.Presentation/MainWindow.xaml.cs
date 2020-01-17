711556306831ba671373a86cffd889195a712766
Jędrzej Pieczyrak <jedrzej.pieczyrak@vtssoftware.com>
2020-01-17 15:47:59 +0100

    Solution

---
+++ EscapeRecruitmentRoom/EscapeRecruitmentRoom.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace EscapeRecruitmentRoom.Presentation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
